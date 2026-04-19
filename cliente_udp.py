028a55fcceed77ad14f4b00b91624d15fcb95966
EmilyMay34 <142463585+EmilyMay34@users.noreply.github.com>
2026-04-18 22:07:21 -0500

    Add files via upload

---
+++ cliente_udp.py
@@ -0,0 +1,23 @@
+import socket
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+cliente.settimeout(3) # Si en 3 segundos no hay respuesta, se asume  que se perdio
+servidor_dir = ('localhost', 6000)
+
+print("--- CLIENTE UDP ---")
+
+while True:
+    msg = input("\nEscribe tu mensaje: ")
+    if msg.lower() == 'salir': break
+    
+    cliente.sendto(msg.encode(), servidor_dir)
+    
+    try:
+        # Intentamos recibir respuesta
+        respuesta, addr = cliente.recvfrom(1024)
+        print(f"Servidor respondio: {respuesta.decode()}")
+    except socket.timeout:
+    
+        print("El paquete se  a perdio en la red (No hubo respuesta por parte del servidor ).")
+
+cliente.close()
